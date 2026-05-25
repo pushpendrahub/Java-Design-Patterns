a59e4bb1c26ce9b61d326b414ac5da06825d945d
Pushpendra <singhpushpendra9326@gmail.com>
2026-05-25 21:25:53 +0530

    Singleton commit

---
+++ src/creational/Singleton.java
@@ -0,0 +1,14 @@
+package creational;
+
+public class Singleton {
+
+    private static Singleton instance;
+
+    public static Singleton getInstace(){
+        if(instance==null){
+            instance=new Singleton();
+        }
+        return instance;
+    }
+
+}
---
+++ src/creational/SingletonMain.java
@@ -0,0 +1,11 @@
+package creational;
+
+public class SingletonMain {
+    public static void main(String[] args){
+        Singleton db1= Singleton.getInstace();
+        Singleton db2= Singleton.getInstace();
+
+        System.out.println(db1.hashCode());
+        System.out.println(db2.hashCode());
+    }
+}
